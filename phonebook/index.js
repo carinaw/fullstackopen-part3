0c084f56c3223c09e661829259278b8b6df2b095
Carina Wetzlhütter <carina.wetzlhuetter@gmail.com>
2023-12-08 15:32:17 +0200

    Add exercises 3.15-3.18

--- phonebook/index.js
+++ phonebook/index.js
@@ -15,66 +15,93 @@ app.use(
 	morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
 
+const errorHandler = (error, request, response, next) => {
+	if (error.name === "CastError") {
+		return response.status(400).send({ error: "malformatted id" });
+	}
+
+	next(error);
+};
+
+app.use(errorHandler);
+
 const persons = [];
 
 app.get("/", (request, response) => {
 	response.send("<h1>Persons</h1>");
 });
 
-app.get("/api/persons", (request, response) => {
+app.get("/api/persons", (request, response, persons) => {
 	Person.find({}).then((persons) => {
 		response.json(persons);
 	});
 });
 
-app.get("/api/persons/:id", (request, response) => {
-	Person.findById(request.params.id).then((person) => {
-		response.json(person);
-		console.log(person);
-	});
-
-	// if (person) {
-	// 	response.json(person);
-	// } else {
-	// 	response.status(404).send("This entry does not exist.");
-	// }
+app.get("/api/persons/:id", (request, response, next) => {
+	Person.findById(request.params.id)
+		.then((person) => {
+			if (person) {
+				response.json(person);
+			} else {
+				response.status(404).end();
+			}
+		})
+		.catch((error) => next(error));
 });
 
 app.get("/info", (request, response) => {
-	const entries = persons.length;
-	const receiveDate = new Date();
-	console.log(receiveDate);
-	console.log(entries);
-	response.send(
-		`<p>Phonebook has info for ${entries} people.<p>${receiveDate}</p>`
-	);
+	Person.countDocuments({})
+		.then((count) => {
+			response.send(
+				`phonebook has info for ${count} people.<br />${new Date()}`
+			);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
 });
 
-// app.delete("/api/persons/:id", (request, response) => {
-// 	const id = Number(request.params.id);
-// 	persons = persons.filter((person) => person.id !== id);
-// 	response.status(204).end();
-// });
+app.delete("/api/persons/:id", (request, response, next) => {
+	Person.findByIdAndDelete(request.params.id)
+		.then((result) => {
+			response.status(204).end();
+		})
+		.catch((error) => next(error));
+});
+
+app.put("/api/persons/:id", (request, response, next) => {
+	const body = request.body;
+
+	const person = {
+		name: body.name,
+		number: body.number,
+	};
+
+	Person.findByIdAndUpdate(request.params.id, person, { new: true })
+		.then((updatedPerson) => {
+			response.json(updatedPerson);
+		})
+		.catch((error) => next(error));
+});
 
 app.post("/api/persons", (request, response) => {
 	const body = request.body;
 
-	if (persons.find((person) => person.name === body.name)) {
-		response.status(400).json({ error: "only one entry per name" });
-	} else if (!body.name) {
-		response.status(400).json({ error: "name missing" });
-	} else if (!body.number) {
-		response.status(400).json({ error: "number missing" });
-	} else {
-		const person = new Person({
-			name: body.name,
-			number: body.number,
-		});
+	// if (persons.find((person) => person.name === body.name)) {
+	// 	response.status(400).json({ error: "only one entry per name" });
+	// } else if (!body.name) {
+	// 	response.status(400).json({ error: "name missing" });
+	// } else if (!body.number) {
+	// 	response.status(400).json({ error: "number missing" });
+	// } else {
+	const person = new Person({
+		name: body.name,
+		number: body.number,
+	});
 
-		person.save().then((savedPerson) => {
-			response.json(savedPerson);
-		});
-	}
+	person.save().then((savedPerson) => {
+		response.json(savedPerson);
+	});
 });
 
 const PORT = process.env.PORT;
